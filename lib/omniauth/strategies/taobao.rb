b7c82da7f3860bb52b9befec4a5450991b1a457b
Fei Yang <cdredfox@gmail.com>
2012-12-02 15:32:41 +0800

    modify

--- lib/omniauth/strategies/taobao.rb
+++ lib/omniauth/strategies/taobao.rb
@@ -27,34 +27,22 @@ module OmniAuth
       end
 
       def raw_info
-        url = 'http://gw.api.taobao.com/router/rest'
+        url = 'https://eco.taobao.com/router/rest'
 
         query_param = {
-          :app_key => options.client_id,
-
-          # TODO to be moved in options
-          # TODO add more default fields (http://my.open.taobao.com/apidoc/index.htm#categoryId:1-dataStructId:3)
           :fields => 'user_id,uid,nick,sex,buyer_credit,seller_credit,location,created,last_visit,birthday,type,status,alipay_no,alipay_account,alipay_account,email,consumer_protection,alipay_bind',
           :format => 'json',
           :method => 'taobao.user.get',
-          :session => @access_token.token,
-          :sign_method => 'md5',
-          :timestamp   => Time.now.strftime('%Y-%m-%d %H:%M:%S'),
+          :access_token => @access_token.token,
           :v => '2.0'
         }
-        query_param = generate_sign(query_param)
         res = Net::HTTP.post_form(URI.parse(url), query_param)
         @raw_info ||= MultiJson.decode(res.body)['user_get_response']['user']
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
       end
       
-      def generate_sign(params)
-        # params.sort.collect { |k, v| "#{k}#{v}" }
-        str = options.client_secret + params.sort {|a,b| "#{a[0]}"<=>"#{b[0]}"}.flatten.join + options.client_secret
-        params['sign'] = Digest::MD5.hexdigest(str).upcase!
-        params
-      end
+ 
     end
   end
 end
