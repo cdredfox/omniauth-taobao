ce0cbb5f4fb1f20589c40e513cb5cea4571d5e15
Yang Fei <cdredfox@gmail.com>
2013-03-05 09:14:44 +0000

    test ok

--- lib/omniauth/strategies/taobao.rb
+++ lib/omniauth/strategies/taobao.rb
@@ -3,10 +3,22 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Taobao < OmniAuth::Strategies::OAuth2
+
+      authorize_url='https://oauth.tbsandbox.com/authorize'
+      token_url='https://oauth.tbsandbox.com/token'
+      
+
+      if Rails.env.production?
+        authorize_url='https://oauth.taobao.com/authorize'
+        token_url='https://oauth.taobao.com/token' 
+      end
+
       option :client_options, {
-        :authorize_url => 'https://oauth.taobao.com/authorize',
-        :token_url => 'https://oauth.taobao.com/token',
+        :authorize_url => authorize_url,
+        :token_url => token_url,
       }
+
+      
       def request_phase
         options[:state] ||= '1'
         super
@@ -27,16 +39,24 @@ module OmniAuth
       end
 
       def raw_info
-        url = 'https://eco.taobao.com/router/rest'
-
         query_param = {
-          :fields => 'user_id,uid,nick,sex,buyer_credit,seller_credit,location,created,last_visit,birthday,type,status,alipay_no,alipay_account,alipay_account,email,consumer_protection,alipay_bind',
+          :fields => 'user_id,uid,nick,sex,buyer_credit,seller_credit,location,created,last_visit,birthday,type,status,alipay_no,alipay_account,alipay_account,consumer_protection,alipay_bind',
           :format => 'json',
           :method => 'taobao.user.get',
           :access_token => @access_token.token,
           :v => '2.0'
         }
-        res = Net::HTTP.post_form(URI.parse(url), query_param)
+        api_url='https://gw.api.tbsandbox.com/router/rest'
+        if Rails.env.production?
+          api_url = 'https://eco.taobao.com/router/rest'  
+        end
+        uri = URI.parse(api_url)
+        uri.query = URI.encode_www_form(query_param)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(uri.request_uri)
+        res = http.request(request)
         @raw_info ||= MultiJson.decode(res.body)['user_get_response']['user']
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
@@ -46,3 +66,9 @@ module OmniAuth
     end
   end
 end
+module OpenSSL
+  module SSL
+    remove_const :VERIFY_PEER
+  end
+end
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
